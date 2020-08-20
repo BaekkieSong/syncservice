e63f57cfe66064377dff4a1ba7c1689f5c0b1d2d
baekkie_song <baekkie_song@tmax.co.kr>
2020-08-20 14:55:56 +0900

    [doc][Protobuf] Google-Protobuf 사용법 추가 작성 * unittest에 반복필드값 생성,삭제 방법 추가 * unittest에 Enum값 사용방법 추가

--- src/google/google-protobuf/proto_unittest.js
+++ src/google/google-protobuf/proto_unittest.js
@@ -1,3 +1,4 @@
+const { assert } = require('console');
 const path = require('path');
 const workspaceDir = path.join(__dirname, '../../..');
 var messages = require(path.join(workspaceDir, 'google/protocol/sync_pb'));
@@ -33,17 +34,23 @@ console.log('share:', message.getShare());
 console.log('getupdates:', message.getGetUpdates());
 console.log('markerlist:', message.getGetUpdates().getFromProgressMarkerList());
 console.log('marker0:', message.getGetUpdates().getFromProgressMarkerList()[0].toObject())
+message.getGetUpdates().getFromProgressMarkerList().splice(1, 2)
+console.log('removedMarkerlist:', message.getGetUpdates().getFromProgressMarkerList())
 message.setMessageContents(proto.sync_pb.ClientToServerMessage.Contents.GET_UPDATES);
 console.log('messageContents:', message.getMessageContents())
 
 let sb = message.serializeBinary();
-console.log(sb);
+//console.log(sb);
 let dsb = proto.sync_pb.ClientToServerMessage.deserializeBinary(sb);
 console.log(dsb.getGetUpdates().getFromProgressMarkerList());
 console.log(typeof(sb));
 let b = dsb.toString();
 b = dsb.toObject();
 console.log(b)
+
+let pbResponse = new proto.sync_pb.ClientToServerResponse();
+pbResponse.setErrorCode(proto.sync_pb.SyncEnums.ErrorType.SUCCESS);
+assert(pbResponse.getErrorCode() == proto.sync_pb.SyncEnums.ErrorType.SUCCESS);
 //let c = Buffer.from(b);
 //console.log(proto.sync_pb.ClientToServerMessage.deserializeBinary(sb).getGetUpdates().getFromProgressMarkerList());
 // console.log(typeof(sb));
